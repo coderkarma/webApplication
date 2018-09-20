21985e14b38a6eceaa959e4ba2d3a9eb60d40d60
karma <karmadrukpa11@gmail.com>
2018-09-19 22:29:33 -0700

    changed js code

--- scripts.js
+++ scripts.js
@@ -1,27 +1,14 @@
-//If you put all the image paths into an array, the rest of the code gets much simpler
-const images = ["images/image1.jpg", "images/image2.jpg", "images/image3.jpg","images/image4.jpg","images/image5.jpg","images/image6.jpg","images/image7.jpg"];
 
-function getRandomInt() {
-  // No longer need min and max parameters, just the length of the array
-  return Math.floor(Math.random() * images.length);
-}
+const body = document.querySelector('body');
+let i = 1;
 
-function nextBackground(){
-  $("body").css("background-image", "url(" +images[getRandomInt()] + ")");
-}
-// For a continuously running timer, user setInterval, instead of setTimeout
-setInterval(nextBackground, 3000);
+setInterval(()=>{
+  body.style.backgroundImage = `url(images/image${i}.jpg)`;
+  i++;
+	if (i === 8) {
+		i = 1;
+  }
+  
+},3000)
 
 
-// const img = document.querySelector("body");
-// const urls = ["images/beach.jpg", "images/haven.jpg"];
-
-// function getRandom() {
-//      return Math.floor(Math.random() * urls.length);
-//  }
-
-//  function changeImage() {
-//     img.body = urls[getRandom()];
-//  }
-
-//  setInterval(changeImage, 2000);
